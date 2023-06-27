8e0bea7b46de8da87ec1847ecfa9c0952a78c405
Manuele Menozzi <manuele.menozzi@gmail.com>
2023-06-27 09:29:52 +0200

    Sets cypress base url to e2e test server

--- src/Resources/app/storefront/test/e2e/cypress.config.js
+++ src/Resources/app/storefront/test/e2e/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://127.0.0.1:8005',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
