f8dc2d8ed4c2f1eea5240e73eb2bc9bf6c3c7897
Manuele Menozzi <manuele.menozzi@gmail.com>
2025-10-23 09:26:56 +0200

    Migrate cypress tests to playwright

--- tests/acceptance/tests/Filter.spec.ts
+++ tests/acceptance/tests/Filter.spec.ts
@@ -1,15 +1,47 @@
 import { test, expect } from './../BaseTestFile';
+import {Manufacturer} from "@shopware-ag/acceptance-test-suite";
 
-test('Filter on startpage', async ({ StorefrontPage, AdminApiContext, DefaultSalesChannel }) => {
-    await StorefrontPage.goto('/');
+test.describe('Filter on startpage', () => {
+    let shopwareManufacturer: Manufacturer;
+    let webgriffeManufacturer: Manufacturer;
 
-    // Filter for manufacturer shopwareAG
-    // ...
-});
+    test.beforeEach(async ({TestDataService}) => {
+        shopwareManufacturer = await TestDataService.createBasicManufacturer({name: 'shopwareAG'});
+        webgriffeManufacturer = await TestDataService.createBasicManufacturer({name: 'Webgriffe'});
 
-test('Filter on startpage 2 with revenge', async ({ StorefrontPage, AdminApiContext, DefaultSalesChannel }) => {
-    await StorefrontPage.goto('/');
+        await TestDataService.createBasicProduct({
+            name: 'First product',
+            manufacturerId: shopwareManufacturer.id,
+        });
+        await TestDataService.createBasicProduct({
+            name: 'Second product',
+        });
+        await TestDataService.createBasicProduct({
+            name: 'Third product',
+        });
+        await TestDataService.createBasicProduct({
+            name: 'Fourth product',
+            manufacturerId: webgriffeManufacturer.id,
+        });
+        await TestDataService.createBasicProduct({
+            name: 'Fifth product',
+            manufacturerId: webgriffeManufacturer.id,
+        });
+    });
 
-    // Filter for manufacturer shopwareAG
-    // ...
+    test('Filter for manufacturer shopwareAG', async ({ StorefrontHome, ShopCustomer, SelectProductFilterOption }) => {
+        await ShopCustomer.goesTo(StorefrontHome.url());
+        await ShopCustomer.expects(StorefrontHome.productItemNames).toHaveCount(5);
+
+        await ShopCustomer.attemptsTo(SelectProductFilterOption(StorefrontHome.manufacturerFilter, shopwareManufacturer.name));
+        await ShopCustomer.expects(StorefrontHome.productItemNames).toHaveCount(1);
+    });
+
+    test('Filter for manufacturer Webgriffe', async ({ StorefrontHome, ShopCustomer, SelectProductFilterOption }) => {
+        await ShopCustomer.goesTo(StorefrontHome.url());
+        await ShopCustomer.expects(StorefrontHome.productItemNames).toHaveCount(5);
+
+        await ShopCustomer.attemptsTo(SelectProductFilterOption(StorefrontHome.manufacturerFilter, webgriffeManufacturer.name));
+        await ShopCustomer.expects(StorefrontHome.productItemNames).toHaveCount(2);
+    });
 });
